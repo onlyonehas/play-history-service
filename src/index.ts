8b3b4479c36ca65d1402e60b05f49f81b3bfe53d
onlyonehas <hassanabbas93@hotmail.com>
2025-10-06 20:55:55 +0100

    refactor: add docs + more tests + stricter validations

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 import express from 'express';
-
-
-import { PlayEvent } from '../src/types/playEvents';
+import { PlayEvent, PlayEventInput } from '../src/types/playEvents';
 import { events } from '../src/storage/events';
-
-const isDuplicate = (event: PlayEvent) =>
-  events.some(e =>
-    e.user_id === event.user_id &&
-    e.content_id === event.content_id &&
-    e.device === event.device &&
-    e.timestamp.getTime() === event.timestamp.getTime()
-  );
+import { isDuplicate } from './helpers/checkDuplicate';
 
 const app = express();
 app.use(express.json());
 
 app.post('/play', (req, res) => {
-  const { user_id, content_id, device, timestamp, playback_duration } = req.body;
-  if (!user_id || !content_id || !device || !timestamp || !playback_duration) {
-    return res.status(400).json({ error: 'Missing fields' });
+  try {
+    const { user_id, content_id, device, timestamp, playback_duration }: PlayEventInput = req.body;
+    if (!user_id || !content_id || !device || !timestamp || !playback_duration) {
+      return res.status(400).json({ error: 'Missing fields' });
+    }
+
+    const date = new Date(timestamp);
+
+    if (isNaN(date.getTime())) {
+      return res.status(400).json({ error: 'Invalid timestamp format' });
+    }
+
+    const input: PlayEvent = { user_id, content_id, device, timestamp: date, playback_duration };
+    if (!isDuplicate(input)) events.push(input);
+    res.status(201).json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
   }
-  const event: PlayEvent = { user_id, content_id, device, timestamp: new Date(timestamp), playback_duration };
-  if (!isDuplicate(event)) events.push(event);
-  res.status(200).json({ success: true });
 });
+
+
 
 export default app;
--- src/types/playEvents.ts
+++ src/types/playEvents.ts
@@ -5,3 +5,10 @@ export interface PlayEvent {
   timestamp: Date;
   playback_duration: number;
 }
+export type PlayEventInput = {
+  user_id: string;
+  content_id: string;
+  device: 'web' | 'mobile' | 'tv';
+  timestamp: string;
+  playback_duration: number;
+};
